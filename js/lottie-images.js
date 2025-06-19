f680b28e91fedf0f930382ed7d87855db213b569
gashpo <gashpofigma@gmail.com>
2025-06-19 16:22:08 +0800

    update lottie import way

--- js/lottie-images.js
+++ js/lottie-images.js
@@ -1,14 +1,7 @@
-var lottieBg = lottie.loadAnimation({
-  container: document.getElementById("lottie-bg"),
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-  path: "images/Shape.json",
-});
-var loadingWhite = lottie.loadAnimation({
-  container: document.getElementById("lottie-loading_white"),
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-  path: "images/Loading_white.json",
-});
+// var lottieBg = lottie.loadAnimation({
+//   container: document.getElementById("lottie-bg"),
+//   renderer: "svg",
+//   loop: true,
+//   autoplay: true,
+//   path: "images/Shape.json",
+// });
